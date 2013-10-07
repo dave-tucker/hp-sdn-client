61acf4b072524cadc11c916474704c18fb5e36ef
Dave Tucker <dave@dtucker.co.uk>
2013-10-07 16:28:46 +0100

    Started refactor and PEP8 compliance

---
+++ hpsdnclient/client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# Copyright (c)  2013 Hewlett-Packard Development Company, L.P.
+#
+# Permission is hereby granted, fpenrlowee of charge, to any person
+# obtaining a copy of this software  and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""This library provides a Python interface to the HP SDN Controller
+API"""
+
+__author__ = 'Dave Tucker, Hewlett-Packard Development Company,'
+__version__ = '0.2.0'
+
+from hpsdnclient.api import ApiBase
+from hpsdnclient.core import CoreMixin
+from hpsdnclient.net import NetMixin
+from hpsdnclient.of import OfMixin
+
+class Api(CoreMixin, OfMixin, NetMixin, ApiBase):
+    """ The container class for the HP SDN Controller Api """
+    def __init__(self, controller, user, password):
+        super(Api, self).__init__(controller, user, password)
